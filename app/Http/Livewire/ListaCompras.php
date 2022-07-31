da86ef9529983c14f8183008cc8089c304fe09a6
edutronicos <amgp@ymail.com>
2022-07-31 17:57:08 -0300

    soma

--- app/Http/Livewire/ListaCompras.php
+++ app/Http/Livewire/ListaCompras.php
@@ -6,8 +6,10 @@ use Livewire\Component;
 
 class ListaCompras extends Component
 {
-    public $item;
-    public $lista = ['Bolo', 'Alface', 'Batata', 'Repolho', 'Coca', 'Fanta'];
+    public $item = '';
+    public $valor= '';
+    public $soma = 0;
+    public $lista = [];
 
     public $key;
     public $acao = 'cadastrar';
@@ -19,15 +21,21 @@ class ListaCompras extends Component
 
     public function add() 
     {
-        array_unshift($this->lista, $this->item); //PHP=> array_unshift: adiciona no inicio da lista
+        $item = $this->item;
+        $valor= $this->valor;
+        $lis = compact('item', 'valor');
+        array_unshift($this->lista, $lis); //PHP=> array_unshift: adiciona no inicio da lista
+        //dd($lis);
         //array_push($this->lista, $this->item); //PHP=> array_push: adiciona no fim da lista
         $this->reset('item');
+        $this->reset('valor');
     }
 
     public function resetlist() 
     {
         unset($this->lista);
         $this->lista = [];
+        $this->reset('soma');
     }
 
     public function delete($key)
@@ -37,15 +45,29 @@ class ListaCompras extends Component
 
     public function edit($key)
     {
+        
         $this->key = $key;
         $this->acao = 'atualizar';
-        $this->item = $this->lista[$key];
+        $this->item = $this->lista[$key]['item'];
+        $this->valor = $this->lista[$key]['valor'];
+        //dd($this->valor);
     }
 
-    public function update()
+    public function update($key)
     {
-        $this->lista = array_replace($this->lista, [$this->key => $this->item]); //PHP=> array_replace: substitui no array lista pelo item referente a key.
+        //dd($this->item);
+        //dd($key);
+        $this->lista = array_replace($this->lista, [$key => ['item' => $this->item, 'valor' => $this->valor]]); //PHP=> array_replace: substitui no array lista pelo item referente a key.
+        //dd($this->lista);
         $this->reset('item');
+        $this->reset('valor');
         $this->acao = 'cadastrar';
+    }
+
+    public function total() 
+    {
+        foreach ($this->lista as $key => $valor ){
+            $this->soma += $valor['valor'];
+        }
     }
 }
--- resources/views/livewire/lista-compras.blade.php
+++ resources/views/livewire/lista-compras.blade.php
@@ -3,31 +3,49 @@
             <div class="form-group input-group mb-3">
                 @if($acao == 'cadastrar')
                     <input class="form-control" type="text" wire:model="item" wire:keydown.enter="add()">
+                    <input class="form-control" type="text" wire:model="valor" wire:keydown.enter="add()">
                 @else
-                    <input class="form-control" type="text" wire:model="item" wire:keydown.enter="update()">
+                    <input class="form-control" type="text" wire:model="item" wire:keydown.enter="update({{$key}})">
+                    <input class="form-control" type="text" wire:model="valor" wire:keydown.enter="update({{$key}})">
                 @endif
 
                 @if($acao == 'cadastrar')
                     <button class="btn btn-primary" wire:click="add()"><i class="bi bi-plus-square"></i></button>
                 @else
-                    <button class="btn btn-primary" wire:click="update()">Atualizar</button>
+                    <button class="btn btn-primary" wire:click="update({{$key}})">Atualizar</button>
                 @endif
             </div>
             
             
             
             <table class="table align-middle">
+                
                 @foreach ($lista as $key => $nome)
                     <tr>
-                        <th scope="row" class="text-start ">{{$nome}}</th>
-                        <td><button class="btn btn-primary" wire:click="edit({{$key}})"><i class="bi bi-pencil"></i></button></td>
+                    
+                        <th scope="row" class="text-start ">{{$nome['item']}}</th>
+                        <td>{{ number_format($nome['valor'], 2, ',', '.') }}</td>
+                        <td class="text-end"><button class="btn btn-primary" wire:click="edit({{$key}})"><i class="bi bi-pencil"></i></button></td>
                         <td class="text-end"><button class="btn btn-primary" wire:click="delete({{$key}})"><i class="bi bi-trash3"></i></button></td>
                     </tr>
                 @endforeach
             </table>
+            
+            <div class="row justify-content-end mt-3">
+                <div class="form-group input-group mb-3" style="width: 200px;">
+                    <input class="form-control"  type="text" wire:model="soma">
+                    <button class="btn btn-primary" wire:click="total">TOTAL</button>
+                </div>
+            </div>
 
             <div class="text-end">
                 <button class="btn btn-primary" wire:click="resetlist()">Limpar Lista - <i class="bi bi-recycle"></i></button>
             </div>
+
+            
+            
 </div>
+
+
+            
 
